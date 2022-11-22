53699b6306fa08b4b428919b094bf5e045f18883
Josh Gragg <69860217+Joshg7777@users.noreply.github.com>
2022-11-22 13:59:01 -0500

    Updated roll and cli

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// import statements
+import minimist from 'minimist';
+import {roll} from "../lib/roll.js";
+
+// parse command line arguments
+const args = minimist(process.argv.slice(2))
+
+let sides = args.sides;
+let dice = args.dice;
+let rolls = args.rolls;
+if(sides < 0 || dice < 0 || rolls < 0){
+    console.log("Please enter positive numbers");
+    process.exit(1);
+}
+let returnedObject = roll(sides, dice, rolls);
+console.log(returnedObject);
+process.exit(0);
